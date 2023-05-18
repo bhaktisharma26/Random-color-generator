ce2dc8b30bdfc4b3403c6d470042161e3bd91b23
Bhakti Sharma <123656846+bhaktisharma26@users.noreply.github.com>
2023-05-18 22:56:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const getColor=() =>{
     const randomCode="#"+randomNumber.toString(16);
     console.log(randomNumber,randomCode);
     document.body.style.backgroundColor=randomCode;
-    document.getElementById("color-code").innerText="You bring colors in my life:))";
+    document.getElementById("color-code").innerText=randomCode;
     navigator.clipboard.writeText(randomCode);
     
 }
